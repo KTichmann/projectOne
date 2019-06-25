c94c79fbff3627c7c8093aff741d28154242c8f6
ktichmann <ktichmann@gmail.com>
2019-06-25 23:20:29 +0200

    add formik and basic registration form

--- packages/web/src/modules/register/views/RegisterView.tsx
+++ packages/web/src/modules/register/views/RegisterView.tsx
@@ -1,12 +1,107 @@
 import React, { PureComponent } from "react";
-import Button from "antd/lib/button";
+import * as yup from "yup";
+import { Form, Icon, Input, Button } from "antd";
+import { withFormik, FormikErrors, FormikProps } from "formik";
 
-export class RegisterView extends PureComponent {
-	render() {
-		return (
-			<div>
-				<Button type='primary'>I'm a button</Button>
-			</div>
-		);
-	}
+interface FormValues {
+  email: string;
+  password: string;
 }
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+
+class C extends PureComponent<FormikProps<FormValues> & Props> {
+  render() {
+    const {
+      values,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      touched,
+      errors
+    } = this.props;
+    return (
+      <form style={{ display: "flex", margin: "auto" }} onSubmit={handleSubmit}>
+        <div className="login-form" style={{ width: 400, margin: "auto" }}>
+          <Form.Item
+            help={touched.email && errors.email ? errors.email : ""}
+            validateStatus={touched.email && errors.email ? "error" : undefined}
+          >
+            <Input
+              name="email"
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Email"
+              onChange={handleChange}
+              value={values.email}
+              onBlur={handleBlur}
+            />
+          </Form.Item>
+          <Form.Item
+            help={touched.password && errors.password ? errors.password : ""}
+            validateStatus={
+              touched.password && errors.password ? "error" : undefined
+            }
+          >
+            <Input
+              name="password"
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+              value={values.password}
+              onBlur={handleBlur}
+            />
+          </Form.Item>
+          <Form.Item>
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Register
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            Or <a href="">login now!</a>
+          </Form.Item>
+        </div>
+      </form>
+    );
+  }
+}
+
+const duplicateEmail = "already taken";
+const emailNotLongEnough = "email must be at least 3 characters";
+const invalidEmail = "email must be a valid email";
+const passwordNotLongEnough = "password must be at least 3 characters";
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail)
+    .required(),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255)
+    .required()
+});
+
+export const RegisterView = withFormik<Props, FormValues>({
+  validationSchema,
+  mapPropsToValues: () => ({ email: "", password: "" }),
+  handleSubmit: async (values, { props, setErrors }) => {
+    const errors = await props.submit(values);
+    if (errors) {
+      setErrors(errors);
+    }
+  }
+})(C);
