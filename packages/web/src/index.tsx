c87d301f4c731c4e91f97e713c02309e54ae6bc5
unknown <ktichmann@gmail.com>
2019-06-25 17:46:56 +0200

    set up react for project

---
+++ packages/web/src/index.tsx
@@ -0,0 +1,17 @@
+import * as serviceWorker from "./serviceWorker";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { ApolloProvider } from "react-apollo";
+import { client } from "./apollo";
+import { Routes } from "./routes";
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<Routes />
+	</ApolloProvider>,
+	document.getElementById("root")
+);
+
+serviceWorker.unregister();
---
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const RegisterConnector = () => (
+	<div>This is the register connector</div>
+);
