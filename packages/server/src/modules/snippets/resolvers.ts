459d7702aef794a18fe66024859486032fee886e
ktichmann <ktichmann@gmail.com>
2019-07-15 23:01:48 +0200

    fix snippet queries

--- packages/server/src/modules/snippets/resolvers.ts
+++ packages/server/src/modules/snippets/resolvers.ts
@@ -1,36 +1,44 @@
 import { ResolverMap } from "../../types/graphql-utils";
 import { Snippet } from "../../entity/Snippet";
 import { createSnippet } from "./functions/createSnippet";
+import { Any } from "typeorm";
 
 export const resolvers: ResolverMap = {
-	Query: {
-		getPublicSnippets: async () => {
-			const res = await Snippet.find({
-				where: { visibility: "public" }
-			});
-			return res;
-		},
-		getUserSnippets: async (userId: string) => {
-			const res = await Snippet.find({
-				where: { userId }
-			});
-			return res;
-		},
-		getSnippetById: async (id: string) => {
-			const res = await Snippet.find({
-				where: { id }
-			});
-			return res;
-		}
-	},
+  Query: {
+    getPublicSnippets: async () => {
+      const res = await Snippet.find({
+        where: { visibility: "public" }
+      });
+      return res;
+    },
+    getUserSnippets: async (_, { userId }) => {
+      const res = await Snippet.find({
+        where: { userId }
+      });
+      return res;
+    },
+    getSnippetById: async (_, { snippetId }) => {
+      const res = await Snippet.findOne({
+        where: { id: snippetId }
+      });
+      return res;
+    }
+    // getSnippetsByTag: async (_, { tag }) => {
+    //   const res = await Snippet.find({
+    //     where: { tags: Any(tag) }
+    //   });
 
-	Mutation: {
-		createSnippet: (
-			_,
-			args: GQL.ICreateSnippetOnMutationArguments,
-			{ session }
-		) => {
-			return createSnippet(session, args);
-		}
-	}
+    //   return res;
+    // }
+  },
+
+  Mutation: {
+    createSnippet: (
+      _,
+      args: GQL.ICreateSnippetOnMutationArguments,
+      { session }
+    ) => {
+      return createSnippet(session, args);
+    }
+  }
 };
