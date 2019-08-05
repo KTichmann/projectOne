ec62b6ee875f3da1e269daa18dd4c7f9db23f87b
ktichmann <ktichmann@gmail.com>
2019-08-05 11:53:00 +0200

    build controller

--- packages/controller/dist/modules/userControllers/MeController/index.d.ts
+++ packages/controller/dist/modules/userControllers/MeController/index.d.ts
@@ -1,6 +1,9 @@
 import * as React from "react";
 interface Props {
-	children: (data: { username: string; id: string }) => JSX.Element | null;
+    children: (data: {
+        username: string;
+        id: string;
+    }) => JSX.Element | null;
 }
 export declare const MeController: React.ComponentClass<Props, any>;
 export {};
