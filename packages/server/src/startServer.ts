e481c035465dda4b03ecd5e3878469e8ff13f895
ktichmann <ktichmann@gmail.com>
2019-08-19 12:29:37 +0200

    test

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -41,13 +41,12 @@ export const startServer = async () => {
 			name: "tid",
 			secret: SESSION_SECRET,
 			resave: false,
-			saveUninitialized: false,
-			cookie: {
-				httpOnly: true,
-				secure: process.env.NODE_ENV === "production",
-				domain: process.env.FRONTEND_HOST,
-				maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-			}
+			saveUninitialized: false
+			// cookie: {
+			// 	httpOnly: true,
+			// 	secure: process.env.NODE_ENV === "production",
+			// 	maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+			// }
 		})
 	);
 
